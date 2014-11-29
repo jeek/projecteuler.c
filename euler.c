d42f0b08a544dbc25017f5fd4a377bcbf71ebc8f
T.J. Eckman <jeek@jeek.net>
2014-11-29 12:25:02 -0500

    Updated v0.1's for argumentless run
    Program was failing when run without arguments. Logic is hard.

--- euler.c
+++ euler.c
@@ -4,7 +4,7 @@
 #define SOLVED [1];
 
 int main(int argc, char* argv[]) {
-    if (argc == 1 | argc >= 2 & atoi(argv[1]) == 1) {
+    if (argc == 1 || (argc >= 2 & atoi(argv[1]) == 1)) {
         if (argc == 3) {
             printf("001: %d\n", problem001(atoi(argv[2])));
         } else {
